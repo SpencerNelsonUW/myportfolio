8a9c37f4e0b27553e3ba0904f4c5a4debdbe7dff
SpencerNelsonUW <spencernelsonn@gmail.com>
2023-03-27 15:00:39 -0700

    restructuring particles

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,7 +1,4 @@
-import React, { useCallback } from 'react';
-import Particles from "react-particles";
-import { loadFull } from "tsparticles";
-import particlesOptions from "../../config/particles.json";
+import Particle from '../Particle/Particle';
 import Avatar from "../../images/Avatar.jpg"
 import Image from 'react-bootstrap/Image';
 import './Home.css'
@@ -11,20 +8,19 @@ import './Home.css'
 
 export const Home = () => {
 
-    const particlesInit = useCallback(main => {
-        loadFull(main);
-    }, [])
+
 
 
     return(
         <div className='homeWrapper' id='home'>
+            <Particle/>
             <div className='homeBody'>
-                <Particles options={particlesOptions} init={particlesInit}/>
                 <h1 className="hi">Hi there,</h1>
                 <Image className="img-fluid rounded-circle avatar" src={Avatar}></Image>
                 <h1 className="hi">my name is Spencer</h1>
                 {/* <BsArrowDownShort href="#navbar" className="downArrow"/> */}
             </div>
+            
         </div>
     )
 
